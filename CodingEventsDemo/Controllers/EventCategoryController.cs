d4611e14ad1d6023e596362eb283ea48d0a19384
Meghan <stlmeghan@yahoo.com>
2022-11-07 17:39:39 -0600

    omg orm exercises

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.title = "All Categories";
+            List<EventCategory> categories = context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
+
